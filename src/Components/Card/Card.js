8d992dd2c08af779d116383f2f364f389597bc6a
mimiicodes <ogunleyeolamide01@gmail.com>
2022-07-15 18:28:14 +0100

    style changes

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -4,7 +4,7 @@ import "./Card.css";
  export const Card = props => (
     <div className="card-container">    
       <img
-        src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`}
+        src={`https://robohash.org/${props.monster.id}?set=set2&size=180x200`}
         alt="monster"
       />
       <h2>{props.monster.name}</h2>
